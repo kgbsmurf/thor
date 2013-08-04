0d58552eb4863a91dd9e4d0327b180fc51eda1cd
Andrey <andreykireev@gmail.com>
2013-08-04 17:27:37 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThorPlatform::Application.config.secret_key_base = '97a19e3b0c16f3b306696906d3ef678b9a59e0218a85a11e789e56036b1240c300abef454f28878f6acc0f97529f31a0cedbcf80a9b13e9e4b1ea251259ed7bf'
